d2d600c70f43118aaadf10d42fd4235785e238e7
dumidu1998 <dumiduraj@gmail.com>
2021-06-22 15:42:57 +0530

    profile card added

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,40 +1,25 @@
 import React from 'react'
 import Navbar from '../layout/Navbar';
-import { DataGrid } from '@material-ui/data-grid';
+import Box from "@material-ui/core/Box";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import '../styles.css'
 import { Button, FormGroup, FormLabel, OutlinedInput } from '@material-ui/core';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import { makeStyles } from '@material-ui/core/styles';
 
-import '../styles.css'
+const useStyles = makeStyles(theme => ({
+    card: {
+        marginLeft: '150px',
+        marginRight: '50px',
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
 
-
-const columns = [
-    { field: 'id', headerName: 'Admin Id', width: 250 },
-    { field: 'firstName', headerName: 'Name', width: 200 },
-    { field: 'lastName', headerName: 'Batch', width: 200 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 190,
-    }
-];
-
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: 'dumidu', age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-const useStyles = makeStyles((theme) => ({
+    },
     modal: {
         display: 'flex',
         alignItems: 'center',
@@ -46,11 +31,25 @@ const useStyles = makeStyles((theme) => ({
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
     },
+    details: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%"
+    },
+    cover: {
+        width: 250,
+        height: 280
+    },
+    controls: {
+        display: "flex",
+        alignItems: "center",
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(1)
+    }
 }));
 
 export default function Admin() {
     const classes = useStyles();
-
     const [openstudent, setOpenstudent] = React.useState(false);
 
 
@@ -66,12 +65,63 @@ export default function Admin() {
         <div className="content">
             <h1>Profile Settings</h1>
             <Navbar />
-            <Button className="adminbtn" variant="contained" color="primary" onClick={handleOpenstudent} style={{ marginLeft: '150px', display: 'block', marginTop: '30px' }}>
-                Add Admin
+            <Card className={classes.card}>
+                <Box boxShadow={3}>
+                    <CardMedia
+                        className={classes.cover}
+                        image="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                        title="Live from space album cover"
+                    />
+                </Box>
+                <div className={classes.details}>
+                    <Typography component="h5" variant="h5">
+                        User Details
+                    </Typography>
+                    <h3>Name:  Dumidu Kasun</h3>
+                    <h3>Name:  Dumidu Kasun</h3>
+                    <h3>Name:  Dumidu Kasun</h3>
+                    <h3>Name:  Dumidu Kasun</h3>
+                </div>
+            </Card>
+            <Button className="adminbtn" variant="contained" color="primary" onClick={handleOpenstudent} style={{ marginRight: '150px', float: 'right', display: 'block', marginTop: '30px' }}>
+                Reset Password
             </Button>
-            <div style={{ height: 600, marginLeft: '110px', marginTop: '10px' }}>
-                <DataGrid rows={rows} columns={columns} pageSize={10} />
-            </div>
-        </div>
+
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={openstudent}
+                onClose={handleClosestudent}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 300,
+                }}
+            >
+                <Fade in={openstudent}>
+                    <div className={classes.paper}>
+                        <form onSubmit={() => alert('submitted')} autocomplete="off" >
+                            <h2 id="transition-modal-title">Add New Admin</h2>
+                            <FormGroup>
+                                <FormLabel>Full Name</FormLabel>
+                                <OutlinedInput type="text" required style={{ height: '30px', marginBottom: '10px' }} />
+                                <FormLabel>Username</FormLabel>
+                                <OutlinedInput type="text" required style={{ height: '30px', marginBottom: '10px' }} />
+                                <FormLabel>Email</FormLabel>
+                                <OutlinedInput type="text" required style={{ height: '30px', marginBottom: '10px' }} />
+                                <FormLabel>Password</FormLabel>
+                                <OutlinedInput type="text" required style={{ height: '30px', marginBottom: '10px' }} />
+                                <Button variant="contained" type='submit' disbled color="primary"
+                                    style={{ marginTop: '30px' }}>
+                                    Add Student
+                                </Button>
+                            </FormGroup>
+                        </form>
+                    </div>
+                </Fade>
+            </Modal>
+        </div >
     )
 }
