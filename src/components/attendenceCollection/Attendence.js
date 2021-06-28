d33e946ad7680b763e1cc42118c7f4f6fc49eb64
dumidu1998 <dumiduraj@gmail.com>
2021-06-28 16:42:09 +0530

    marking done

--- src/components/attendenceCollection/Attendence.js
+++ src/components/attendenceCollection/Attendence.js
@@ -12,10 +12,13 @@ export default function Attendence() {
     const [Id, setId] = useState('');
     const [btndisabled, setbtndisabled] = useState(false);
     const [name, setname] = useState('');
+    const [mainId, setmainId] = useState('')
     var type = "";
     const [d, setD] = useState(new Date().valueOf())
     const [msg, setmsg] = useState('')
     const [warn, setwarn] = useState('')
+
+
     useEffect(() => {
         const timeout = setTimeout(() => {
             setD(new Date().valueOf());
@@ -39,53 +42,86 @@ export default function Attendence() {
                 }
             })
             .catch(function (error) {
-                // handle error
                 console.log(error);
             })
     }, [d]);
 
-    function markStudent(sid) {
+
+    async function markStudent(sid) {
+        setmainId(sid);
+        const stdRef = db.collection('students');
+        const queryRef1 = stdRef.where('rfid', '==', Id);
+        const res1 = await queryRef1.get();
+        res1.forEach(doc => {
+            setname(doc.data().name);
+        })
+
         db.collection('studentAttendence').add({
-            id: SidenavHeader,
+            id: sid,
             date: new Date()
         })
-        console.log("done")
     }
 
-    function MarkStaff(sid) {
+    async function MarkStaff(sid) {
+        setmainId(sid);
+        const stfRef = db.collection('staff');
+        const queryRef2 = stfRef.where('rfid', '==', Id);
+        const res2 = await queryRef2.get();
+        res2.forEach(doc => {
+            setname(doc.data().name);
+        })
         db.collection('staffAttendence').add({
             id: sid,
             date: new Date()
         })
-        console.log("done")
-
     }
 
     async function checkID() {
         const rfidsRef = db.collection('rfids');
         const queryRef = rfidsRef.where('rfid', '==', Id);
         const res = await queryRef.get();
+        if (res.empty) {
+            setmsg('ID Not Registered')
+            setwarn('red');
+            return false;
+        }
         console.log(res);
         res.forEach(doc => {
             type = doc.data().type;
             type == "student" ? (markStudent(doc.data().id)) : (MarkStaff(doc.data().id));
         })
-        // type == "student" ? (markStudent()) : (MarkStaff());
+
+
     }
 
-    const submitfunction = (e) => {
-        e.preventDefault();
-        document.getElementById('mcard').style.display = "block";
-        checkID();
-        probar();
+    const requestatt = () => {
+        setmsg('Attendence Request Sent!')
+        setwarn('green');
+        db.collection('requests').add({
+            id: mainId,
+            date: new Date(),
+            status: false,
+            type: type
+        })
+    }
 
+    const submitfunction = async (e) => {
+        e.preventDefault();
+        var out = await checkID();
+        if (out == false) {
+            probar();
+            return 0;
+        } else {
+            document.getElementById('mcard').style.display = "block";
+            probar();
+        }
     }
 
     var i;
     const probar = () => {
         i = setInterval(() => {
             setProgress((prevProgress) => (prevProgress <= 10 ? 0 : prevProgress - 10));
-        }, 250) //200
+        }, 200) //200
         if (progress == 0) {
             clearInterval(i);
         }
@@ -94,12 +130,6 @@ export default function Attendence() {
     if (progress === 10) {
         window.location.reload();
     }
-
-    if (btndisabled == true) {  //disable according to detection
-        setbtndisabled(false);
-    }
-
-
 
     return (
         <div>
@@ -114,11 +144,12 @@ export default function Attendence() {
             </Grid>
             <img src="https://www.felca.org/wp-content/uploads/Logo-edex-pdf.jpg" width="130" />
             <h1 id="hh">Welcome to Edex Institute</h1>
+            {name}
             <div class="">
                 <div className={warn}>{msg}</div>
                 <form autoComplete="off" onSubmit={submitfunction}>
                     <h4>Enter your ID number</h4>
-                    <TextField type="number" id="outlined-basic" label="ID Number" required variant="outlined" value={Id}
+                    <TextField autoFocus type="number" id="outlined-basic" label="ID Number" required variant="outlined" value={Id}
                         onChange={(e) => setId(e.target.value)} />
                     <br /><br />
                     <Button disabled={btndisabled} type="submit" variant="outlined" color="primary">
@@ -131,10 +162,10 @@ export default function Attendence() {
                 <Card className="cardclass" variant="outlined" id="mcard">
                     <CardContent>
                         <Typography className="" variant="h4">
-                            Dumidu Kasun Bandara Rajakaruna
+                            {name}
                         </Typography>
                         <Typography variant="h5" component="h2">
-                            2018/IS/064
+                            {mainId}
                         </Typography>
                         <Typography variant="h6" component="h2">
                             Attendence Marked
@@ -143,7 +174,7 @@ export default function Attendence() {
                             <img src="https://www.jing.fm/clipimg/full/24-242581_checkmark-svg-clipart-green-tick-png.png"
                                 width="100" />
                         </Typography>
-                        <Button type="submit" variant="outlined" color="primary">
+                        <Button type="submit" variant="outlined" color="primary" onClick={requestatt}>
                             Request Attendence Report
                         </Button>
                     </CardContent>
