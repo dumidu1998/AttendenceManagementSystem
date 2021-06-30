aeb2d6d42f341eebf40372cb1c86b954ebda6874
dumidu1998 <dumiduraj@gmail.com>
2021-06-30 21:47:54 +0530

    batch.js edited

--- src/components/batch/Batch.js
+++ src/components/batch/Batch.js
@@ -7,7 +7,6 @@ import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
-
 import '../styles.css'
 import { db } from '../../firebase';
 
